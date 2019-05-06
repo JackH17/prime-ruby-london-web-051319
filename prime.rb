368eeecf828560ec7c199ed34696a351facb26b3
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-06 17:10:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,12 @@ def prime?(number)
   first = 2 
   
   if number > 1 
-    range = (first..number-1)
+    range = (first..number-1).to_a
   
 number % number == 0 
     return true
+  else 
+    return false
 
 end
   
